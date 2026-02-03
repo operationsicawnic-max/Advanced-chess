9b0778bf3387c6874cfefae5db4869b918a933f0
operationsicawnic-max <operations.icawnic@gmail.com>
2026-02-03 17:26:24 +0530

    Create timer.js

---
+++ js/timer.js
@@ -0,0 +1,21 @@
+import { state } from "./state.js";
+
+export function startTimer(updateUI) {
+  stopTimer();
+  state.timer = setInterval(() => {
+    if (state.game.turn() === 'w') state.whiteTime--;
+    else state.blackTime--;
+    updateUI();
+  }, 1000);
+}
+
+export function stopTimer() {
+  if (state.timer) {
+    clearInterval(state.timer);
+    state.timer = null;
+  }
+}
+
+export function formatTime(sec) {
+  return Math.floor(sec / 60) + ":" + String(sec % 60).padStart(2, "0");
+}
